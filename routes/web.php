7dd1b11c7b1392f9ad1770d96299ef6379572b93
Bashar-collab <mohammadkalla17@gmail.com>
2025-04-24 02:03:37 +0300

    added log api

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,9 @@ Route::get('/', function () {
     return ['message' => __('messages.welcome')];
 });
 
+Route::get('/logs', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
+
+
 require __DIR__.'/auth.php';
